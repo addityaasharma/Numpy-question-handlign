d2367327d56fae19bcbc309c8b0c617a9c31871d
Aditya Sharma <adityasharma96655@gmail.com>
2025-02-06 20:23:52 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([[4, 9, 2], [16, 25, 36]])
+# ✅ The sum of all elements.
+# ✅ The mean of all elements.
+# ✅ The median of all elements.
+# ✅ The standard deviation.
+# ✅ The variance.
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.median(arr))
+print(np.std(arr))
+print(np.var(arr))
