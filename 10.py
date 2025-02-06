b18fd942739a252e542ec69f3191c531fd137b93
Aditya Sharma <adityasharma96655@gmail.com>
2025-02-06 20:25:44 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([1, np.e, 10, 100])
+
+# ✅ The exponential of each element.
+# ✅ The natural logarithm of each element.
+# ✅ The square root of each element.
+
+print(np.exp(arr))
+print(np.log(arr))
+print(np.sqrt(arr))
